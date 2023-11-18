5088f6f4b249f0299bc8a0ec63ee8a1b8d8f7b76
NguyenThiYenNhii <nhintyph33288@fpt.edu.vn>
2023-11-18 16:38:57 +0700

    database

--- Main1.sql
+++ Main1.sql
@@ -67,7 +67,7 @@ DROP TABLE PhieuGiaoCa
 GO
 	
 CREATE TABLE PhieuGiaoCa(
-	ID_PGK INT PRIMARY KEY,
+	ID_PGK INT IDENTITY(1,1) PRIMARY KEY,
 	ID_NV INT,
 	TenNV NVARCHAR(50),
 	TGBD DATETIME,
@@ -89,7 +89,7 @@ DROP TABLE PhieuChamCong
 GO
 
 CREATE TABLE PhieuChamCong (
-    ID_Phieu INT PRIMARY KEY,
+    ID_Phieu INT IDENTITY(1,1) PRIMARY KEY,
     ID_NV INT,
     NgayCham DATE,
     GioVao TIME,
@@ -559,7 +559,7 @@ DROP TABLE DanhGia
 GO
 
 CREATE TABLE DanhGia(
-ID_DG INT PRIMARY KEY,
+ID_DG INT IDENTITY(1,1) PRIMARY KEY,
 ID_KH INT,
 MoTa NVARCHAR(250),
 TrangThai BIT
@@ -573,7 +573,7 @@ DROP TABLE TinNhan
 GO
 
 CREATE TABLE TinNhan(
-ID_TinNhan INT PRIMARY KEY,
+ID_TinNhan INT IDENTITY(1,1) PRIMARY KEY,
 ID_NV INT,
 ID_KH INT,
 NoiDungChat NVARCHAR(250),
@@ -598,6 +598,332 @@ MoTa NVARCHAR(250)
 )
 GO
 
+--THÊM DỮ LIỆU
+
+-- Chức Vụ
+INSERT INTO ChucVu (TenCV, MoTa)
+VALUES 
+  (N'Quản lý', N'Quản lý nhân sự'),
+  (N'Nhân viên', N'Nhân viên bán hàng'),
+  (N'Kế toán', N'Quản lý tài chính'),
+  (N'Lao công', N'Công việc lao động'),
+  (N'Bảo vệ', N'Bảo vệ cửa hàng');
+
+SELECT * FROM ChucVu
+
+-- Lương
+INSERT INTO Luong (ID_NV, MucLuong, NgayTra, MoTa)
+VALUES 
+  (1, 5000000, '2023-11-30', N'Lương tháng 11/2023'),
+  (2, 4500000, '2023-11-30', N'Lương tháng 11/2023'),
+  (3, 6000000, '2023-11-30', N'Lương tháng 11/2023'),
+  (4, 4000000, '2023-11-30', N'Lương tháng 11/2023'),
+  (5, 3500000, '2023-11-30', N'Lương tháng 11/2023');
+
+SELECT * FROM Luong
+
+-- Thưởng
+INSERT INTO Thuong (ID_NV, NgayThuong, MoTa)
+VALUES 
+  (1, '2023-11-15', N'Thưởng tháng 11/2023'),
+  (2, '2023-11-15', N'Thưởng tháng 11/2023'),
+  (3, '2023-11-15', N'Thưởng tháng 11/2023'),
+  (4, '2023-11-15', N'Thưởng tháng 11/2023'),
+  (5, '2023-11-15', N'Thưởng tháng 11/2023');
+
+SELECT * FROM Thuong
+
+-- Phiếu Giao Ca
+INSERT INTO PhieuGiaoCa (ID_NV, TenNV, TGBD, TGKT, TienMatDauCa, TienMatCuoiCa, TienCK, TienGG, TongTien, Note)
+VALUES
+(1, N'Nguyễn Văn A', '2023-11-18 08:00:00', '2023-11-18 17:00:00', 500000, 600000, 20000, 10000, 590000, N'Giao hàng tới cửa hàng ABC'),
+(2, N'Trần Thị B', '2023-11-18 09:30:00', '2023-11-18 18:30:00', 600000, 700000, 25000, 12000, 680000, N'Giao hàng tới cửa hàng XYZ'),
+(3, N'Lê Văn C', '2023-11-18 10:15:00', '2023-11-18 19:45:00', 550000, 650000, 18000, 9000, 630000, N'Giao hàng tới cửa hàng LMN'),
+(4, N'Phạm Thị D', '2023-11-18 08:45:00', '2023-11-18 17:45:00', 520000, 610000, 22000, 11000, 600000, N'Giao hàng tới cửa hàng PQR'),
+(5, N'Hoàng Văn E', '2023-11-18 09:00:00', '2023-11-18 18:00:00', 480000, 580000, 19000, 9500, 560500, N'Giao hàng tới cửa hàng UVW');
+
+SELECT * FROM PhieuGiaoCa
+
+-- Phiếu Chấm Công
+INSERT INTO PhieuChamCong (ID_NV, NgayCham, GioVao, GioRa, GhiChu)
+VALUES
+(1, '2023-11-18', '08:00:00', '17:00:00', N'Giờ làm bình thường'),
+(2, '2023-11-18', '09:30:00', '18:30:00', N'Giờ làm thêm'),
+(3, '2023-11-18', '10:15:00', '19:45:00', N'Làm thêm cho dự án'),
+(4, '2023-11-18', '08:45:00', '17:45:00', N'Nghỉ buổi sáng'),
+(5, '2023-11-18', '09:00:00', '18:00:00', N'Thuộc đợt đào tạo');
+
+SELECT * FROM PhieuChamCong
+
+-- Nhân Viên
+INSERT INTO NhanVien (ID_CV, TenNV, Email, MatKhau, GioiTinh, NgaySinh, DiaChi)
+VALUES
+(1, N'Nguyễn Văn A', 'a@example.com', 'password123', 1, '1990-01-15', N'123 Đường ABC'),
+(2, N'Trần Thị B', 'b@example.com', 'securepass', 0, '1995-05-20', N'456 Đường XYZ'),
+(3, N'Lê Văn C', 'c@example.com', 'pass123', 1, '1988-09-08', N'789 Đường LMN'),
+(4, N'Phạm Thị D', 'd@example.com', 'd_securepass', 0, '1992-03-25', N'456 Đường PQR'),
+(5, N'Hoàng Văn E', 'e@example.com', 'pass456', 1, '1993-12-12', N'789 Đường UVW');
+
+SELECT * FROM NhanVien
+
+-- Tài Khoản
+INSERT INTO TaiKhoan (ID_NV, TenTK, MatKhau, VaiTro, TrangThai)
+VALUES
+(1, 'a_account', 'a_password', 1, 1),
+(2, 'b_account', 'b_securepass', 0, 1),
+(3, 'c_account', 'c_pass123', 1, 1),
+(4, 'd_account', 'd_securepass', 0, 1),
+(5, 'e_account', 'e_pass456', 1, 1);
+
+SELECT * FROM TaiKhoan
+
+-- Khách Hàng
+INSERT INTO KhachHang (TenKH, MatKhau, GioiTinh, NgaySinh, DiaChi, DienThoai, Email, Hinh, MoTa)
+VALUES
+(N'Khách hàng 1', 'khachhang1pass', 1, '1988-03-10', N'789 Đường Khách Hàng', '1234567890', 'khachhang1@example.com', 'avatar1.jpg', N'Khách hàng thường xuyên'),
+(N'Khách hàng 2', 'khachhang2pass', 0, '1992-07-25', N'456 Đường Người Dùng', '9876543210', 'khachhang2@example.com', 'avatar2.jpg', N'Người mua hàng thường xuyên'),
+(N'Khách hàng 3', 'khachhang3pass', 1, '1985-11-18', N'123 Đường Thành Viên', '5556667777', 'khachhang3@example.com', 'avatar3.jpg', N'Khách hàng VIP'),
+(N'Khách hàng 4', 'khachhang4pass', 0, '1990-05-08', N'789 Đường Premium', '9998887777', 'khachhang4@example.com', 'avatar4.jpg', N'Thành viên độc quyền'),
+(N'Khách hàng 5', 'khachhang5pass', 1, '1993-09-30', N'456 Đường Gold', '3332221111', 'khachhang5@example.com', 'avatar5.jpg', N'Thành viên Vàng');
+
+SELECT * FROM KhachHang
+
+-- Hóa Đơn
+INSERT INTO HoaDon (ID_KH, ID_NV, ID_TT, ID_VC, ID_GH, ID_CT, ID_GG, ID_DGG, NgayDat, NgayCan, NgayGiao, TenKH, DiaChi, PhiVanChuyen, TongTien, TongTienSauGiam, TrangThai, GhiChu)
+VALUES
+(1, 1, 1, 1, 1, 1, 1, 1, '2023-11-18', '2023-11-20', '2023-11-21', N'Khách hàng 1', N'789 Đường Khách Hàng', 30000, 500000, 480000, N'Chờ thanh toán', N'Đang đợi thanh toán'),
+(2, 2, 2, 2, 2, 2, 2, 2, '2023-11-18', '2023-11-20', '2023-11-22', N'Khách hàng 2', N'456 Đường Người Dùng', 25000, 600000, 575000, N'Đã giao hàng', N'Đang trên đường giao'),
+(3, 3, 3, 3, 3, 3, 3, 3, '2023-11-18', '2023-11-20', '2023-11-23', N'Khách hàng 3', N'123 Đường Thành Viên', 35000, 700000, 670000, N'Đã giao hàng', N'Đã nhận hàng với sự hài lòng'),
+(4, 4, 1, 2, 4, 4, 4, 4, '2023-11-18', '2023-11-20', '2023-11-24', N'Khách hàng 4', N'789 Đường Premium', 40000, 800000, 760000, N'Đã giao hàng', N'Khách hàng yêu cầu giao hàng nhanh chóng'),
+(5, 5, 2, 1, 5, 5, 5, 5, '2023-11-18', '2023-11-20', '2023-11-25', N'Khách hàng 5', N'456 Đường Gold', 30000, 550000, 520000, N'Chờ thanh toán', N'Đang xử lý thanh toán');
+
+SELECT * FROM HoaDon
+
+-- Chi Tiết Hóa Đơn
+INSERT INTO ChiTietHoaDon (ID_HD, ID_SP, ID_GG, ID_GH, ID_DGG, TenSP, DonGia, SoLuong)
+VALUES
+(1, 1, 1, 1, 1, N'Áo phông nam trắng', 150000, 2),
+(2, 2, 2, 2, 2, N'Áo phông nữ đen', 300000, 1),
+(3, 3, 3, 3, 3, N'Áo phông unisex xanh', 200000, 1),
+(4, 4, 1, 2, 4, N'Áo phông nam đỏ', 400000, 1),
+(5, 5, 2, 1, 5, N'Áo phông nữ vàng', 100000, 3);
+
+SELECT * FROM ChiTietHoaDon
+
+---MÀU SẮC
+INSERT INTO MauSac (TenMS, Hinh, TrangThai, MoTa) VALUES
+(N'Đen', 'den.jpg', 1, N'Màu đen cho áo phông'),
+(N'Trắng', 'trang.jpg', 1, N'Màu trắng cho áo phông'),
+(N'Xanh dương', 'xanh_duong.jpg', 1, N'Màu xanh dương cho áo phông'),
+(N'Đỏ', 'do.jpg', 1, N'Màu đỏ cho áo phông'),
+(N'Xám', 'xam.jpg', 1, N'Màu xám cho áo phông');
+
+SELECT * FROM MauSac
+
+---KÍCH THƯỚC
+INSERT INTO KichThuoc (TenKT, GiaTriKT, TrangThai, MoTa) VALUES
+('S', 'Small', 55, N'Kích thước nhỏ cho áo phông'),
+('M', 'Medium', 60, N'Kích thước trung bình cho áo phông'),
+('L', 'Large', 66, N'Kích thước lớn cho áo phông'),
+('XL', 'Extra Large', 70, N'Kích thước cực lớn cho áo phông'),
+('XXL', 'Double Extra Large', 67, N'Kích thước rất lớn cho áo phông');
+
+SELECT * FROM KichThuoc
+
+---THỂ LOẠI
+INSERT INTO TheLoai (TenTL, Hinh, TrangThai, MoTa) VALUES 
+(N'Áo phông nam', 'ao_phong_nam.jpg', 1, N'Các loại áo phông dành cho nam giới'),
+(N'Áo phông nữ', 'ao_phong_nu.jpg', 1, N'Các loại áo phông dành cho nữ giới'),
+(N'Áo phông trẻ em', 'ao_phong_tre_em.jpg', 1, N'Các loại áo phông dành cho trẻ em'),
+(N'Áo phông Unisex', 'ao_phong_unisex.jpg', 1, N'Các loại áo phông có thể dùng cho cả nam và nữ giới'),
+(N'Áo phông thể thao', 'ao_phong_the_thao.jpg', 1, N'Các loại áo phông thích hợp cho hoạt động thể thao');
+
+SELECT * FROM TheLoai
+
+---CHẤT LIỆU
+
+INSERT INTO ChatLieu (ID_SP, TenCL, Hinh, TrangThai, MoTa) VALUES 
+(1, N'Cotton', 'cotton.jpg', 1, N'Chất liệu cotton thông thường'),
+(2, N'Len', 'len.jpg', 1, N'Chất liệu len dày dặn và ấm áp'),
+(3, N'Polyester', 'polyester.jpg', 1, N'Chất liệu polyester bền và chống nhăn'),
+(4, N'Satin', 'satin.jpg', 1, N'Chất liệu satin mềm mại và lịch lãm'),
+(5, N'Kaki', 'kaki.jpg', 1, N'Chất liệu kaki bền và thoải mái');
+
+SELECT * FROM ChatLieu
+
+--- HÌNH ẢNH
+INSERT INTO HinhAnh VALUES
+(1, 'https://example.com/hinh1.jpg', 'jpg', 102.4),
+(2, 'https://example.com/hinh2.png','png', 51.2),
+(3, 'https://example.com/hinh3.jpg','jpg', 72.0),
+(4, 'https://example.com/hinh4.png','png', 96.0),
+(5, 'https://example.com/hinh5.jpg','jpg', 48.0);
+
+SELECT * FROM HinhAnh
+
+---SẢN PHẨM
+INSERT INTO SanPham (ID_NCC, ID_MS, ID_TL, ID_CL, ID_KT, ID_GG, ID_HA, TenSP, TheLoai, KichThuoc, MauSac, ChatLieu, SoLuong, DonGia, NgaySanXuat, SoLuongTon, SoLuotXem, TrangThai, MoTa) VALUES
+(1, 1, 1, 1, 1, 1, 1, N'Áo phông nam màu đen', N'Áo phông nam', 'S', N'Đen',  N'Cotton', 50, 200000, '2023-01-01', 1, 100, 1, N'Áo phông nam màu đen thông thoáng và thoải mái'),
+(2, 2, 2, 2, 2, 1, 2, N'Áo phông nam màu trắng', N'Áo phông nữ', 'M', N'Trắng',  N'Len', 45, 180000, '2023-01-02', 1, 190, 1, N'Áo phông nam màu trắng đơn giản và thời trang'),
+(3, 2, 3, 3, 3, 2, 3, N'Áo phông nam màu xanh dương', N'Áo phông trẻ em', 'L', N'Trắng',  N'Polyester', 35, 300000, '2023-01-03', 1, 109, 1, N'Áo phông nam màu xanh dương năng động và trẻ trung'),
+(4, 4, 4, 4, 2, 2, 4, N'Áo phông nam màu đỏ', N'Áo phông Unisex', 'M', N'Đỏ',  N'Satin', 15, 10000, '2023-01-04', 5, 10, 1, N'Áo phông nam màu đỏ cá tính và phong cách'),
+(5, 2, 5, 5, 2, 3, 5, N'Áo phông nữ màu xám', N'Áo phông thể thao', 'M', N'Trắng',  N'Kaki', 50, 200000, '2023-01-05', 1, 100, 1, N'Áo phông nữ màu xám nhẹ nhàng và duyên dáng');
+
+SELECT * FROM SanPham
+
+-- LoGo
+INSERT INTO LoGo (TenLG, Hinh, NgayTao, MoTa)
+VALUES
+('A-Style Logo', 'a_style_logo.jpg', '2023-11-18', 'A-Style: Chất lượng và Phá Cách.'),
+('Chic Trends', 'chic_trends_logo.png', '2023-11-18', 'Chic Trends: Đi theo xu hướng thời trang.'),
+('Unique Styles', 'unique_styles_logo.jpg', '2023-11-18', 'Unique Styles: Phong cách độc đáo và sáng tạo.'),
+('Cool Vibe', 'cool_vibe_logo.png', '2023-11-18', 'Cool Vibe: Phong cách năng động và mới lạ.'),
+('Dynamic Fashion', 'dynamic_fashion_logo.jpg', '2023-11-18', 'Dynamic Fashion: Năng động và thời trang.');
+
+SELECT * FROM LoGo
+
+-- Người Liên Lạc
+INSERT INTO NguoiLienLac (TenNLL, DienThoai, ChucVu, Email, GhiChu)
+VALUES
+('Anna Nguyen', '1234567890', 'Sales Specialist', 'anna_nguyen@example.com', 'Đội trưởng Kinh Doanh.'),
+('Bryan Tran', '9876543210', 'Chief Accountant', 'bryan_tran@example.com', 'Chuyên viên Kế Toán.'),
+('Catherine Cuong', '5556667777', 'HR Manager', 'catherine_cuong@example.com', 'Quản lý Nhân Sự.'),
+('Daniel Lan', '9998887777', 'IT Consultant', 'daniel_lan@example.com', 'Chuyên viên Tư Vấn IT.'),
+('Eva Chau', '3332221111', 'Marketing Expert', 'eva_chau@example.com', 'Chuyên Gia Marketing.');
+
+SELECT * FROM NguoiLienLac
+
+-- Nhà Cung Cấp
+INSERT INTO NhaCungCap (ID_LG, ID_NLL, ID_SP, TenCT, MaLG, MaNLL, Email, DienThoai, DiaChi, MoTa)
+VALUES
+(1, 1, 1, N'Công ty Sản Xuất Áo Nam NamNam', 101, 201, 'namnam@example.com', '1234567890', N'123 Đường ABC, Quận 1, TP.HCM', N'Cung cấp áo nam phong cách'),
+(2, 2, 2, N'Công ty Thời Trang Bờ Vai Xinh', 102, 202, 'bovaixinh@example.com', '9876543210', N'456 Đường XYZ, Quận 2, TP.HCM', N'Cung cấp áo phông thời trang và nổi bật'),
+(3, 3, 3, N'Công ty Áo Phông CoolStyle', 103, 203, 'coolstyle@example.com', '5556667777', N'789 Đường LMN, Quận 3, TP.HCM', N'Cung cấp áo phông với phong cách cool'),
+(4, 4, 4, N'Công ty Duy Fashion Trend', 104, 204, 'duyfashiontrend@example.com', '9998887777', N'Đường 4, Quận Gò Vấp, TP.HCM', N'Cung cấp áo phông nam nữ theo xu hướng'),
+(5, 5, 5, N'Công ty Áo Thể Thao Energetic', 105, 205, 'energetic@example.com', '3332221111', N'Đường 5, Quận 1, TP.HCM', N'Cung cấp áo thể thao năng động cho giới trẻ');
+
+SELECT * FROM NhaCungCap
+
+-- Địa Chỉ
+INSERT INTO DiaChi (TenDC, TenNguoiNhan, DienThoai, NoiNhan, GhiChu)
+VALUES
+(N'123 Đường ABC, Quận 1, TP.HCM', N'Nguyễn Văn Nam', '1234567890', N'Quận 1, TP.HCM', N'Địa chỉ giao hàng mặc định cho khách hàng Nam'),
+(N'456 Đường XYZ, Quận 2, TP.HCM', N'Trần Thị Bình', '9876543210', N'Quận 2, TP.HCM', N'Địa chỉ giao hàng mặc định cho khách hàng Bình'),
+(N'789 Đường LMN, Quận 3, TP.HCM', N'Phạm Văn Cường', '5556667777', N'Quận 3, TP.HCM', N'Địa chỉ giao hàng mặc định cho khách hàng Cường'),
+(N'Đường 4, Quận Gò Vấp, TP.HCM', N'Nguyễn Thị Duyên', '9998887777', N'Quận Gò Vấp, TP.HCM', N'Địa chỉ giao hàng mặc định cho khách hàng Duyên'),
+(N'Đường 5, Quận 1, TP.HCM', N'Lê Văn Eros', '3332221111', N'Quận 1, TP.HCM', N'Địa chỉ giao hàng mặc định cho khách hàng Eros');
+
+SELECT * FROM DiaChi
+
+-- Phiếu Giao Hàng
+INSERT INTO PhieuGiaoHang (ID_SP, ID_KH, ID_NV, ID_HD, NgayGiao, ID_DC, TrangThaiGH, TrangThaiTT, GhiChu)
+VALUES
+(1, 1, 1, 1, '2023-11-20', 1, 0, 0, N'Giao hàng cho đơn hàng #1'),
+(2, 2, 2, 2, '2023-11-21', 2, 0, 0, N'Giao hàng cho đơn hàng #2'),
+(3, 3, 3, 3, '2023-11-22', 3, 0, 0, N'Giao hàng cho đơn hàng #3'),
+(4, 4, 4, 4, '2023-11-23', 4, 0, 0, N'Giao hàng cho đơn hàng #4'),
+(5, 5, 5, 5, '2023-11-24', 5, 0, 0, N'Giao hàng cho đơn hàng #5');
+
+SELECT * FROM PhieuGiaoHang
+
+-- Phiếu Giảm Giá
+INSERT INTO PhieuGiamGia (ID_KH, ID_SP, ID_TSP, ID_TKH, NgayBatDau, NgayHH, GiaTriGG, TrangThai, NgayTao, NgaySuDung, MoTa)
+VALUES
+(1, 1, 1, 1, '2023-11-20', '2023-12-31', '10%', 1, '2023-11-18', NULL, N'Phiếu giảm giá cho khách hàng Nam'),
+(2, 2, 2, 2, '2023-11-21', '2023-12-31', '15%', 1, '2023-11-18', NULL, N'Phiếu giảm giá cho khách hàng Bình'),
+(3, 3, 3, 3, '2023-11-22', '2023-12-31', '20%', 1, '2023-11-18', NULL, N'Phiếu giảm giá cho khách hàng Cường'),
+(4, 4, 4, 4, '2023-11-23', '2023-12-31', '25%', 1, '2023-11-18', NULL, N'Phiếu giảm giá cho khách hàng Duyên'),
+(5, 5, 5, 5, '2023-11-24', '2023-12-31', '30%', 1, '2023-11-18', NULL, N'Phiếu giảm giá cho khách hàng Eros');
+
+SELECT * FROM PhieuGiamGia
+
+-- Phiếu Giảm Giá Theo SP
+INSERT INTO GiamGiaTheoSP (ID_SP, GiaTriGG, DieuKienGG, MoTa)
+VALUES
+(1, '10%', N'Áp dụng cho tất cả sản phẩm', N'Giảm giá 10% cho tất cả sản phẩm áo phông'),
+(2, '15%', N'Áp dụng cho sản phẩm mới', N'Giảm giá 15% cho sản phẩm áo phông mới'),
+(3, '20%', N'Áp dụng cho hóa đơn từ 1 triệu đồng', N'Giảm giá 20% cho hóa đơn từ 1 triệu đồng'),
+(4, '25%', N'Áp dụng cho sản phẩm nam', N'Giảm giá 25% cho sản phẩm áo phông nam'),
+(5, '30%', N'Áp dụng cho khách hàng VIP', N'Giảm giá 30% cho khách hàng VIP');
+
+SELECT * FROM GiamGiaTheoSP
+
+-- Phiếu Giảm Giá Theo Khách Hàng
+INSERT INTO GiamGiaTheoKH (ID_KH, VaiTro, GiaTriGG, DieuKienGG, MoTa)
+VALUES
+(1, 1, '5%', N'Áp dụng cho khách hàng thân thiết', N'Giảm giá 5% cho khách hàng thân thiết'),
+(2, 0, '7%', N'Áp dụng cho tất cả khách hàng', N'Giảm giá 7% cho tất cả khách hàng'),
+(3, 1, '10%', N'Áp dụng cho khách hàng đặc biệt', N'Giảm giá 10% cho khách hàng đặc biệt'),
+(4, 0, '8%', N'Áp dụng cho tất cả khách hàng', N'Giảm giá 8% cho tất cả khách hàng'),
+(5, 1, '12%', N'Áp dụng cho khách hàng VIP', N'Giảm giá 12% cho khách hàng VIP');
+
+SELECT * FROM GiamGiaTheoKH
+
+-- Đợt Giảm Giá
+INSERT INTO DotGiamGia (TeNDGG, NgayBatDau, NgayKetThuc, LoaiGiamGia, GiaTriGG, DieuKienGiamGia, MoTa)
+VALUES
+(N'Đợt giảm giá cuối năm', '2023-11-25', '2023-12-31', N'Giảm giá theo tỷ lệ', '20%', N'Hóa đơn từ 500,000 đồng trở lên', N'Đợt giảm giá cuối năm áp dụng cho tất cả sản phẩm'),
+(N'Đợt giảm giá đầu năm', '2024-01-01', '2024-01-31', N'Giảm giá theo tỷ lệ', '15%', N'Hóa đơn từ 300,000 đồng trở lên', N'Đợt giảm giá đầu năm áp dụng cho sản phẩm mới'),
+(N'Đợt giảm giá mùa hè', '2024-06-01', '2024-08-31', N'Giảm giá theo số tiền', '50,000 đồng', N'Hóa đơn từ 1,000,000 đồng trở lên', N'Đợt giảm giá mùa hè áp dụng cho tất cả sản phẩm'),
+(N'Đợt giảm giá mùa đông', '2024-12-01', '2025-02-28', N'Giảm giá theo số tiền', '30,000 đồng', N'Hóa đơn từ 800,000 đồng trở lên', N'Đợt giảm giá mùa đông áp dụng cho sản phẩm nam'),
+(N'Đợt giảm giá sinh nhật', '2024-09-15', '2024-09-15', N'Giảm giá theo số tiền', '100,000 đồng', N'Hóa đơn từ 500,000 đồng trở lên', N'Đợt giảm giá sinh nhật áp dụng cho khách hàng VIP');
+
+SELECT * FROM DotGiamGia
+
+-- Phương Thức Vận Chuyển
+INSERT INTO PhuongThucVanChuyen (HinhThucVC, MoTa)
+VALUES
+(N'Giao hàng nhanh', N'Giao hàng trong vòng 24 giờ'),
+(N'Giao hàng tiết kiệm', N'Giao hàng trong vòng 3-5 ngày'),
+(N'Giao hàng thông thường', N'Giao hàng trong vòng 7 ngày'),
+(N'Giao hàng quốc tế', N'Giao hàng quốc tế trong vòng 14 ngày'),
+(N'Giao hàng siêu tốc', N'Giao hàng trong vòng 12 giờ');
+
+SELECT * FROM PhuongThucVanChuyen
+
+-- Phương Thức Thanh Toán
+INSERT INTO PhuongThucThanhToan (HinhThucTT, TrangThaiTT, MoTa)
+VALUES
+(N'Thanh toán khi nhận hàng', N'Đã thanh toán', N'Thanh toán khi nhận hàng và đã thanh toán'),
+(N'Thanh toán trực tuyến', N'Chưa thanh toán', N'Thanh toán trực tuyến và chưa thanh toán'),
+(N'Thanh toán qua ví điện tử', N'Đã thanh toán', N'Thanh toán qua ví điện tử và đã thanh toán'),
+(N'Thanh toán qua thẻ tín dụng', N'Chưa thanh toán', N'Thanh toán qua thẻ tín dụng và chưa thanh toán'),
+(N'Thanh toán sau', N'Chưa thanh toán', N'Thanh toán sau khi nhận hàng');
+
+SELECT * FROM PhuongThucThanhToan
+
+-- Đánh Giá
+INSERT INTO DanhGia (ID_KH, MoTa, TrangThai)
+VALUES
+(1, N'Sản phẩm rất tốt, hài lòng với chất lượng', 1),
+(2, N'Giao hàng nhanh chóng, sản phẩm đúng mô tả', 1),
+(3, N'Sản phẩm đẹp, giá cả hợp lý', 1),
+(4, N'Nhân viên phục vụ nhiệt tình, giao hàng đúng hẹn', 1),
+(5, N'Sản phẩm chất lượng, đóng gói cẩn thận', 1);
+
+SELECT * FROM DanhGia
+
+-- Tin Nhắn
+INSERT INTO TinNhan (ID_NV, ID_KH, NoiDungChat, TrangThai, LichSu)
+VALUES
+(1, 1, N'Chào anh Nam, có gì em giúp được không?', 1, '2023-11-18 10:30:00'),
+(2, 2, N'Xin chào Bình, có cần tư vấn thêm không?', 1, '2023-11-18 11:15:00'),
+(3, 3, N'Chào anh Cường, sản phẩm mới đã về, anh quan tâm không ạ?', 1, '2023-11-18 12:00:00'),
+(4, 4, N'Xin chào chị Duyên, có sản phẩm mới chị quan tâm không?', 1, '2023-11-18 14:30:00'),
+(5, 5, N'Chào Eros, đã nhận được đơn hàng của anh, cảm ơn anh đã mua sắm!', 1, '2023-11-18 15:45:00');
+
+SELECT * FROM TinNhan
+
+-- Loại Giảm Giá
+INSERT INTO LoaiGiamGia (ID_DGG, TenLGG, SoTienGiam, DKApDung, MoTa)
+VALUES
+(1, N'Theo tỷ lệ', 10.00, N'Hóa đơn từ 500,000 đồng trở lên', N'Giảm giá 10% cho hóa đơn từ 500,000 đồng trở lên'),
+(2, N'Theo tỷ lệ', 15.00, N'Hóa đơn từ 300,000 đồng trở lên', N'Giảm giá 15% cho hóa đơn từ 300,000 đồng trở lên'),
+(3, N'Theo số tiền', 50000.00, N'Hóa đơn từ 1,000,000 đồng trở lên', N'Giảm giá 50,000 đồng cho hóa đơn từ 1,000,000 đồng trở lên'),
+(4, N'Theo số tiền', 30000.00, N'Hóa đơn từ 800,000 đồng trở lên', N'Giảm giá 30,000 đồng cho hóa đơn từ 800,000 đồng trở lên'),
+(5, N'Theo số tiền', 100000.00, N'Hóa đơn từ 500,000 đồng trở lên', N'Giảm giá 100,000 đồng cho hóa đơn từ 500,000 đồng trở lên');
+
+SELECT * FROM LoaiGiamGia
 
 --THÊM RÀNG BUỘC
 
@@ -677,3 +1003,5 @@ ALTER TABLE dbo.HoaDon
 
 ALTER TABLE dbo.ChatLieu
     ADD FOREIGN KEY (ID_SP) REFERENCES dbo.SanPham(ID_SP);
+
+
